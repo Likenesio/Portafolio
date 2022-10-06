640a8b3fadb5599e3f5336fe52c145fb4cd9d014
Valentin Oliva <Valentinubb@gmail.com>
2022-10-06 17:27:39 -0300

    iconos y tarjetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,34 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'portafolio';
-  content: any=[]
-  loading: boolean=true;
-  constructor (){
-    }
-  ngOnInit(): void{
-      setTimeout(()=>{
+  content: any = [];
+  loading: boolean = true;
+  constructor() {}
+  ngOnInit(): void {
+    setTimeout(() => {
       this.setData();
-  }, 3000);
-}
+    }, 3000);
+  }
   setData() {
-      this.loading = false;
-      this.content=[{
-      title: 'Formacion Academica',
-      img: 'https://ubiobio.cl/w/img/Escudo-UBB.svg',
-      date: '[2014-2022]',
-      Text: 'Ingenieria Civil Informatica'
-    },
-    {
-      title: 'Experiencia Laboral',
-      img:  'assets/biopal.PNG',
-      date: '[2020-2022]',
-      Text: 'Digitalizar boletas y facturas'
-
-    }
-     
-  ]}
+    this.loading = false;
+    this.content = [
+      {
+        title: 'Formacion Academica',
+        img: 'https://ubiobio.cl/w/img/Escudo-UBB.svg',
+        date: '[2014-2022]',
+        Text: 'Ingenieria Civil Informatica',
+      },
+      {
+        title: 'Experiencia Laboral',
+        img: 'assets/biopal.PNG',
+        date: '[2020-2022]',
+        Text: 'Digitalizar boletas y facturas',
+      },
+    ];
+  }
 }
